2c767f6cbd56c81f643c844a92dd8fed7532e439
Dmitrij <sdma7@mail.ru>
2023-03-02 13:46:50 +0300

    view

---
+++ view/CarBuilder.java
@@ -0,0 +1,23 @@
+package view;
+
+import model.Car;
+
+public interface CarBuilder {
+
+    
+    public CarBuilder fixChassis();
+
+    
+    public CarBuilder fixBody();
+
+    
+    public CarBuilder paint();
+
+    
+
+    public CarBuilder fixInterior();
+
+    
+
+    public Car build();
+}
